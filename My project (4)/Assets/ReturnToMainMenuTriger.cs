a3b15d6410143cfd55d9f8be031d3a32a4e6198f
Bone-228 <kostia.mamai@gmail.com>
2026-06-05 19:22:39 +0200

    geting player back to menu after tutorial

---
+++ My project (4)/Assets/ReturnToMainMenuTriger.cs
@@ -0,0 +1,27 @@
+using Assets.scripts_camera;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMainMenuTriger : MonoBehaviour
+{
+    [Header("Scene")]
+    public string mainMenuSceneName = "MainMenu";
+
+    public PlayerMovement playerMovement;
+    public ThirdPersonCam playerCamera;
+    public GameObject uiCursor;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerMovement.enabled = false; // Disable player movement
+            playerCamera.enabled = false; // Disable player camera control
+            uiCursor.SetActive(true); // Show cursor
+            Cursor.lockState = CursorLockMode.None; // Unlock cursor
+            Cursor.visible = false; // Make cursor visible
+            Time.timeScale = 1f;
+
+            SceneManager.LoadScene(mainMenuSceneName);
+        }
+    }
+}
